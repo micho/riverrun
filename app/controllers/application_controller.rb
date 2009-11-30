0578a2dd3f4dbd0339df375349c85f22e1ae6f16
Pablo Villalba <pablo@saiku.es>
2009-11-30 15:39:28 +0100

    Requiring login for the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   include Authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  
+  before_filter :login_required
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
